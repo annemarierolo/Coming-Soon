6a6ea398e0a3a0050aeeb2710c915e9565617c41
annemarierolo <annemarierolo@gmail.com>
2022-03-14 17:01:56 -0400

    Adding input alert

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,14 @@
 function isEmpty() {
+    var input = document.getElementById("input-container").classList;
+    var errorMessage = document.getElementById("error-message");
+    var iconError = document.getElementById("icon");
     if (document.getElementById("input").value.length == 0) {
-        alert("empty")
+        input.add('error');
+        errorMessage.style.visibility = 'visible';
+        iconError.style.visibility = 'visible';
+    } else {
+        input.remove('error');
+        errorMessage.style.visibility = 'hidden';
+        iconError.style.visibility = 'hidden';
     }
 }
